88b6e9f0ea95bdca619c1b53fc3995f7fb9a5339
Roksana St <160417317+Roksana-St@users.noreply.github.com>
2024-10-04 22:18:02 +0300

    add hw6

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
-import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { selectContacts } from './redux/contactsSlice';
-import { persistStore } from 'redux-persist';
+import { selectNameFilter } from './redux/filtersSlice';
 import styles from './App.module.css';
 
 const App = () => {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const getFilteredContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
 
   return (
     <div className={styles.container}>
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
-      <ContactList />
+      <ContactList contacts={filteredContacts} />
     </div>
   );
 };
